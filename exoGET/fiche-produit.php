01de7b14cbc73578d55309c91afa989cedbb68e1
Omar Jabour <jabour.omar@hotmail.com>
2026-04-16 10:26:19 +0200

    :truck: Frontend: exercice 6 v   :truck:

--- exoGET/fiche-produit.php
+++ exoGET/fiche-produit.php
@@ -2,19 +2,15 @@
 session_start();
 
 $id = $_GET['id'] ?? "";
-
 $produitTrouve = null;
-
-
 foreach ($_SESSION['produits'] as $p) {
     if ($p['id'] == $id) {
         $produitTrouve = $p;
-    }
+  }
 }
 ?>
 
 <?php if ($produitTrouve) { ?>
-
     <h2><?php echo $produitTrouve['nom']; ?></h2>
     <img src="<?php echo $produitTrouve['image']; ?>" width="200"><br>
     <p><?php echo $produitTrouve['description']; ?></p>
@@ -22,7 +18,5 @@ foreach ($_SESSION['produits'] as $p) {
     <a href="boutique.php">Retour</a>
 
 <?php } else { ?>
-
     <p>Produit introuvable</p>
-
 <?php } ?>
