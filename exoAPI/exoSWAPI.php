0208db29329973a30709a4d2b2ea6365cfa0e4bd
Omar Jabour <jabour.omar@hotmail.com>
2026-04-17 11:35:27 +0200

    :truck: Frontend: exercice COOKIE  v3  :truck:

---
+++ exoAPI/exoSWAPI.php
@@ -0,0 +1,23 @@
+<?php
+
+// Test technique : 
+
+// EXERCICE : 
+//  ----------------------------------
+/*
+
+// API StarWars : SWAPI : https://swapi.dev/
+
+    - Créer un formulaire permettant de faire une recherche de personnages via l'API Star Wars selon les informations saisies dans un champ de texte.
+
+    - Le nom des personnages récupérés seront affichés sous forme de liste.
+
+    - Chaque élément de la liste sera cliquable
+
+    - Lorsqu'on clique sur un élément de la liste, les informations du personnage associé s'afficheront en dessous de celui-ci. 
+
+    - Les informations à afficher sont : la taille, le poids, la couleur des cheveux, la couleur des yeux, l'année de naissance, le genre
+
+    - Bonus : Afficher le nom de la planète d'origine
+
+*/
